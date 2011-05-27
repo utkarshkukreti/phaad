f3d07dfb63d3eabdda56ce61c1aba58e19efb087
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-05-27 15:21:43 +0530

    Make the CLI support entering multiple line expressions.

--- lib/phaad/cli.rb
+++ lib/phaad/cli.rb
@@ -9,17 +9,33 @@ module Phaad
     def repl
       require 'readline'
 
-      puts "Type exit to exit."
-      while (line = Readline::readline('> ')) != "exit"
-        begin
-          Readline::HISTORY.push line
-          puts Phaad::Generator.new(line).emitted
-        rescue Exception => e
-          puts e
-          p e.message
-          puts e.backtrace
+      puts "Type exit or press Ctrl-D to exit."
+      lines = ""
+      loop do
+        prompt = lines.size == 0 ? '> ' : '>> '
+        line = Readline::readline(prompt)
+
+        exit if line.nil? || line == "exit" && lines.size == 0
+
+        lines << line + "\n"
+        Readline::HISTORY.push line
+
+        if valid_expression?(lines)
+          begin
+            puts Phaad::Generator.new(lines).emitted
+          rescue Exception => e
+            puts e
+            p e.message
+            puts e.backtrace
+          ensure
+            lines = ""
+          end
         end
       end
+    end
+
+    def valid_expression?(lines)
+      !!Ripper.sexp(lines)
     end
   end
 end
